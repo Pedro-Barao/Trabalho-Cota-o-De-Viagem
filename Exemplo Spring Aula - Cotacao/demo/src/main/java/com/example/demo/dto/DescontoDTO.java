8042cad8a04e819e72b57b890411be245bed7bf6
S-thetheus-W <matheus190206@gmail.com>
2026-05-06 21:44:18 -0300

    Add files via upload

--- Exemplo Spring Aula - Cotacao/demo/src/main/java/com/example/demo/dto/DescontoDTO.java
+++ Exemplo Spring Aula - Cotacao/demo/src/main/java/com/example/demo/dto/DescontoDTO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotBlank;
@@ -16,18 +17,21 @@ public class DescontoDTO {
 
     private Long id;
 
-    @ManyToMany
-    @JoinColumn(name = "Cotacao_Id")
-    @NotBlank(message = "O id de cotação é obrigatório")
+    
+    @NotNull(message = "O id de cotação é obrigatório")
     private Long cotacaoId;
 
-    @NotBlank(message = "O valor do desconto é obrigatório")
+    @NotNull(message = "Informe o valor do desconto")
+    @DecimalMin(value = "0.01", message = "O valor do desconto deve ser maior que zero")
+    @DecimalMax(value = "0.5", message = "O valor máximo do desconto é 0.5")
     private BigDecimal valorDesconto;
 
     @NotBlank(message = "A descrição é obrigatória")
+    @Size(min = 5, max = 600, message = "A descrição deve conter entre 5 e 600 caracteres")
     private String descricao;
 
-    @NotBlank(message = "A data de aplicação é obrigat")
+    @NotNull(message = "A data de aplicação é obrigatória")
     private LocalDateTime dataAplicacao;
+
     
-}
+} 
