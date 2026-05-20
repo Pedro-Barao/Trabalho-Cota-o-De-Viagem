94f25e4bf32f0e44cd92b67a28f04fb18de602ed
Guilherme Cortez Lopes <guilhermecortezlopes20@gmail.com>
2026-05-20 19:18:33 -0300

    Update ClienteService.java

--- Exemplo Spring Aula - Cotacao/demo/src/main/java/com/example/demo/service/ClienteService.java
+++ Exemplo Spring Aula - Cotacao/demo/src/main/java/com/example/demo/service/ClienteService.java
@@ -1,5 +1,8 @@
 package com.example.demo.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -7,7 +10,7 @@ import org.springframework.web.server.ResponseStatusException;
 import com.example.demo.dto.ClienteDTO;
 import com.example.demo.mapper.ClienteMapper;
 import com.example.demo.Entities.Cliente;
-import com.example.demo.repository.IClienteRepository;
+import com.example.demo.repository.ClienteRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,27 +18,31 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ClienteService {
 
-    private final IClienteRepository clienteRepository;
-    
-
+    private final ClienteRepository clienteRepository;
     private final ClienteMapper clienteMapper; 
 
+   
     public ClienteDTO cadastrarCliente(ClienteDTO clienteDTO) {
-        
-     
         if (clienteRepository.findByEmail(clienteDTO.getEmail()).isPresent()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Já existe um cliente com este e-mail.");
         }
-
         if (clienteRepository.findByDocumento(clienteDTO.getDocumento()).isPresent()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Já existe um cliente com este documento.");
         }
 
-    
         Cliente novoCliente = clienteMapper.toEntity(clienteDTO);
-
-      
         Cliente clienteSalvo = clienteRepository.save(novoCliente);
+        return clienteMapper.toDTO(clienteSalvo);
+    }
+
+    
+    public List<ClienteDTO> listarTodos() {
+        List<Cliente> clientes = clienteRepository.findAll();
+        return clientes.stream()
+                .map(clienteMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
 
         
         return clienteMapper.toDTO(clienteSalvo);
